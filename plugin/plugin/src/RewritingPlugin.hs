e7c4f03f9eee3fd9784a5f3cc59373d0964bf994
Andrew Kvapil <viluon@espiv.net>
2021-04-09 13:45:51 +0200

    Stage changes before switch to TH

--- plugin/plugin/src/RewritingPlugin.hs
+++ plugin/plugin/src/RewritingPlugin.hs
@@ -2,6 +2,8 @@
 {-# OPTIONS_GHC -Wincomplete-patterns -Wunused-binds -Wunused-matches #-}
 {- HLINT ignore "Use record patterns" -}
 
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
 module RewritingPlugin (plugin) where
 
 import System.IO.Unsafe (unsafePerformIO)
@@ -21,6 +23,7 @@ import qualified GHC.ThToHs as GHC
 import qualified RnExpr     as GHC
 import qualified TcExpr     as GHC
 import qualified Desugar    as GHC
+import qualified Bag        as GHC
 
 import qualified GHC
 
@@ -43,6 +46,8 @@ import Debug.Trace (trace)
 import Control.Monad.State.Class ()
 import Control.Monad.Trans.State.Strict (StateT(runStateT), get, put)
 import Control.Monad.Trans.Class (MonadTrans(lift))
+-- containers
+import qualified Data.HashSet as S
 
 plugin :: GHC.Plugin
 plugin =  GHC.defaultPlugin { GHC.typeCheckResultAction = const install {- this is to get rid of CLI options -}
@@ -64,9 +69,6 @@ type Match      = GHC.Match      GHC.GhcTc LExpr
 type MatchGroup = GHC.MatchGroup GHC.GhcTc LExpr
 type LExpr      = GHC.LHsExpr    GHC.GhcTc
 type Expr       = GHC.HsExpr     GHC.GhcTc
-
-entryExpr :: TH.ExpQ
-entryExpr = [| trace "rewriting-plugin says hello!" True |]
 
 -- | Check that an expression has the expected type.
 -- By Matthew Pickering as shown in plugin-constraint.
@@ -107,7 +109,9 @@ tc ty expr = do
   -- typecheck
   typecheckExpr ty expr_rn
 
-data WrapperState = WS { ws_fun :: Maybe GHC.Name -- ^ the function we're inside of
+data WrapperState = WS { ws_fun         :: Maybe GHC.Name     -- ^ the function we're inside of
+                       , ws_callCounter :: Maybe GHC.Id       -- ^ the reference to the Int denoting the number of the current function call
+                       , ws_processed   :: !(S.HashSet LExpr) -- ^ the set of already processed expressions
                        }
 
 -- type GenericM m = forall a. Data a => a -> m a
@@ -137,36 +141,110 @@ closureType c = case c of
   GHC.ArrWordsClosure  {} -> "bytearray#"
   _                       -> "!unknown"
 
-traceArg :: String -> String -> a -> a
-traceArg funName arg x = unsafePerformIO $ do
+data TraceSort = ArgTrace | EntryTrace deriving (Eq, Show)
+
+logt :: TraceSort -> [String] -> IO ()
+logt sort params = do
+  MkSystemTime {systemNanoseconds = time} <- getSystemTime
+  appendFile "/tmp/trace.csv"
+    . (++ "\n")
+    . concatMap (++ ",")
+    . ([show time, show sort] ++)
+    $ params
+
+traceEntry :: a -> a
+traceEntry = id
+
+traceArg :: String -> String -> Int -> a -> a
+traceArg funName arg callNumber x = unsafePerformIO $ do
   let x' = GHC.asBox x
   closure <- GHC.getBoxedClosureData x'
-  MkSystemTime {systemNanoseconds = time} <- getSystemTime
 
-  appendFile "/tmp/trace.csv" $ (++ "\n") $ concatMap (++ ",") [show time, funName, arg, closureType closure]
+  logt ArgTrace
+    [ funName
+    , show callNumber
+    , arg
+    , closureType closure
+    ]
   return x
 
--- TODO: use optics
+isMatch :: Match -> Bool
+isMatch GHC.Match {} = True
+isMatch _ = False
+
+collectFunInfo :: Bind -> StateT WrapperState GHC.TcM Bind
+collectFunInfo bind =
+  let name = case bind of
+        GHC.FunBind  {} -> Right $ GHC.varName . GHC.unLoc . GHC.fun_id $ bind
+        GHC.PatBind  {} -> Left  "pat bind"
+        GHC.VarBind  {} -> Right $ GHC.varName . GHC.var_id $ bind
+        GHC.AbsBinds {} -> Left  "abs bind"
+        _               -> Left  . ("unsupported Bind extension:\n" ++) . GHC.showSDocUnsafe . GHC.ppr $ bind
+  in case trace "------ collectFunInfo! " name of
+    Right !nm -> trace (indentWithRangle . show . GHC.occNameFS . GHC.occName $ nm) $
+                  do
+                    state <- get
+                    put state {ws_fun = Just nm}
+                    return bind
+    Left  msg -> trace ("  (" ++ msg ++ ")") $ return bind
+
+incrementCallCounter :: LExpr -> StateT WrapperState GHC.TcM LExpr
+incrementCallCounter lexpr@(GHC.L span expr)
+  | {- TODO: come up with a more robust solution -} span /= GHC.noSrcSpan = do
+  counterName <- lift $ GHC.newName $ GHC.mkOccName GHC.dataName "call-counter"
+  let counterId = GHC.mkLocalId counterName GHC.intTy
+
+  -- let matchGroup :: MatchGroup = GHC.MG
+  --       { GHC.mg_ext    = GHC.MatchGroupTc
+  --                         { GHC.mg_arg_tys = error "to-do" -- :: [Type]  Types of the arguments, t1..tn
+  --                         , GHC.mg_res_ty  = error "to-do" -- :: Type    Type of the result, tr
+  --                         }
+  --       , GHC.mg_alts   = error "to-do" -- :: Located [LMatch GhcTc LExpr]
+  --       , GHC.mg_origin = GHC.Generated
+  --       }
+
+  let nef = GHC.NoExtField
+
+  let matchGroup = GHC.MG
+        { GHC.mg_ext    = GHC.NoExtField
+        , GHC.mg_alts   = error "to-do"
+        , GHC.mg_origin = GHC.Generated
+        }
+
+  let actualBind :: GHC.HsBindLR GHC.GhcRn GHC.GhcRn = GHC.FunBind
+        { GHC.fun_ext     = GHC.emptyNameSet -- locally-bound free variables of this definition
+        , GHC.fun_id      = error "to-do"
+        , GHC.fun_matches = matchGroup
+        , GHC.fun_co_fn   = GHC.WpHole       -- identity coercion
+        , GHC.fun_tick    = []               -- ticks to put on the rhs
+        }
+
+  let binds = GHC.HsValBinds nef (GHC.ValBinds nef (GHC.unitBag (GHC.L GHC.noSrcSpan actualBind)) [])
+  let letExpr = GHC.HsLet nef (GHC.L GHC.noSrcSpan binds) lexpr
+  -- TODO update state
+  return $ GHC.L GHC.noSrcSpan letExpr
+
+incrementCallCounter x = return x
+
+-- goal: rewrite argument references to go through trace
+-- plan:
+--   - [x] capture all bindings
+--   - [x] rewrite refs
+--   - [x] propagate scope (! misleading, we care about the surrounding function name) to ref sites
+--     - how do we do this?
+--   - [x] use ghc-heap-view to peek inside the arguments
+--   - [ ] add a global mutable map
+--   - [ ] count function calls via the map
+
 rewrite :: GHC.LHsBinds GHC.GhcTc -> GHC.TcM (GHC.LHsBinds GHC.GhcTc)
-rewrite binds = do
-  entry <- tc GHC.boolTy entryExpr
-  let ep_guard = GHC.BodyStmt (error "element type of the rhs") entry (error "should be ?") (error "should be noSyntaxExpr")
-  rewriteIdRefs
-
+rewrite binds = fst <$> (`runStateT` initialState) (everywhereM' trans binds)
   where
-  -- goal: rewrite argument references to go through trace
-  -- plan:
-  --   - [x] capture all bindings
-  --   - [x] rewrite refs
-  --   - [x] propagate scope (! misleading, we care about the surrounding function name) to ref sites
-  --     - how do we do this?
-  --   - [x] use ghc-heap-view to peek inside the arguments
-  --   - [ ] add a global mutable map
-  --   - [ ] count function calls via the map
 
-  isMatch :: Match -> Bool
-  isMatch GHC.Match {} = True
-  isMatch _ = False
+  initialState = WS {ws_fun = Nothing, ws_callCounter = Nothing, ws_processed = S.empty}
+
+  -- the monadic transformation capturing function info, introducing call number variables, and wrapping argument references
+  trans :: Typeable a => a -> StateT WrapperState GHC.TcM a
+  trans = mkM collectFunInfo `extM` wrapRef `extM` incrementCallCounter
 
   boundVars = do
     match <- listify isMatch binds
@@ -204,33 +282,13 @@ rewrite binds = do
       extractIds (GHC.CoPat    _ _ pat _)                = extractIds pat
       extractIds  GHC.XPat      {}                       = unsupportedExtensionOf "Pat"
 
-  rewriteIdRefs :: GHC.TcM (GHC.LHsBinds GHC.GhcTc)
-  rewriteIdRefs = do
-    (binds, _) <- (`runStateT` WS {ws_fun = Nothing}) $ everywhereM' trans binds
-    return binds
-
-    where
-      -- the monadic transformation capturing function info and wrapping argument references
-      trans :: Typeable a => a -> StateT WrapperState GHC.TcM a
-      trans = mkM collectFunInfo `extM` wrapRef
-
-  collectFunInfo :: Bind -> StateT WrapperState GHC.TcM Bind
-  collectFunInfo bind = let name = case bind of
-                                    GHC.FunBind  {GHC.fun_id = lid} -> Right $ GHC.varName . GHC.unLoc $ lid
-                                    GHC.PatBind  {}                 -> Left  "pat bind"
-                                    GHC.VarBind  {GHC.var_id = vid} -> Right $ GHC.varName vid
-                                    GHC.AbsBinds {}                 -> Left  "abs bind"
-                                    _                               -> Left  . ("unsupported Bind extension:\n" ++) . GHC.showSDocUnsafe . GHC.ppr $ bind
-                        in case trace "------ collectFunInfo! " name of
-                            Right !nm -> trace (indentWithRangle . show . GHC.occNameFS . GHC.occName $ nm)
-                                          put WS {ws_fun = Just nm} >>= (const . return $ bind)
-                            Left  msg -> trace ("  (" ++ msg ++ ")") $ return bind
-
   wrapRef :: LExpr -> StateT WrapperState GHC.TcM LExpr
   wrapRef (GHC.L span v@(GHC.HsVar x lid))
     -- make sure the Id is bound by some pattern and it's not a part of an already rewritten expression
     | GHC.unLoc lid `elem` boundVars && span /= GHC.noSrcSpan = do
-      WS {ws_fun = Just funName} <- get
+      WS { ws_fun = Just funName
+         , ws_callCounter = Just callCounter
+         } <- get
 
       let argName = occNameStr . GHC.varName . GHC.unLoc $ lid
       let varWithoutSrcSpan = GHC.L GHC.noSrcSpan v
@@ -249,12 +307,18 @@ rewrite binds = do
 
       -- Typecheck the new expression and capture generated constraints
       (unwrapped_expr, wanteds) <- lift $
-        GHC.captureConstraints (GHC.tcMonoExpr expr_rn (GHC.Check (trace (GHC.showSDocUnsafe (GHC.ppr ty)) ty)))
+        GHC.captureConstraints (GHC.tcMonoExpr expr_rn (GHC.Check ty))
       -- Create the wrapper
       wrapper <- lift $ GHC.mkWpLet . GHC.EvBinds . GHC.evBindMapBinds . snd
                   <$> GHC.runTcS (GHC.solveWanteds wanteds)
       -- Apply the wrapper
-      let final_expr = GHC.mkHsApp (GHC.mkLHsWrap wrapper unwrapped_expr) varWithoutSrcSpan
+      -- GHC.mkHsLam (pats :: [LPat]) (body :: LHsExpr)
+      -- GHC.mkHsWrapPat HsWrapper Pat (GhcPass id) Type
+
+      let counter :: LExpr = GHC.L GHC.noSrcSpan $ GHC.HsVar GHC.NoExtField $ GHC.L GHC.noSrcSpan callCounter
+
+      let final_expr = GHC.mkHsApp
+            (GHC.mkHsApp (GHC.mkLHsWrap wrapper unwrapped_expr) varWithoutSrcSpan) counter
       -- Zonk to instantiate type variables
       lift $ GHC.zonkTopLExpr (trace "------ wrapRef!" final_expr)
 
